24d1a59208961bfd24104e8500ff6c2445b8c1c0
Ivan <ivan.kotov@2011.auditory.ru>
2022-01-26 18:05:34 +0300

    delete python files

--- exercise1.py
+++
@@ -1,7 +0,0 @@
-my_list_1 = [2, 5, 8, 2, 12, 12, 4]
-my_list_2 = [2, 7, 12, 3]
-
-my_list_1 = set(my_list_1)
-my_list_2 = set(my_list_2)
-
-print(my_list_1 - my_list_2)
--- exercise3.py
+++
@@ -1,6 +0,0 @@
-my_list_1 = [2, 2, 5, 12, 8, 2, 12]
-my_list_2 = []
-for i in my_list_1:
-    if my_list_1.count(i)==1:
-        my_list_2.append(i)
-print(my_list_2)
